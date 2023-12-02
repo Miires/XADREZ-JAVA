31a0b117bfb11d7200fa32b537aea19f70e4e8ca
Tamires Silva <tamiressilva7657818@gmail.com>
2023-12-02 14:19:38 -0300

    primeira posição

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,9 +1,13 @@
 package aplicacao;
 
+import tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("ola");
+		
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 	}
 
 }
